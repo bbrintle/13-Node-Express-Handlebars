6223a694369fd8fd9fa56ae71f2d494f86ed095d
bbrintle <bbrintle@gmail.com>
2020-11-05 14:20:48 -0800

    testing heroku connections

--- config/connection.js
+++ config/connection.js
@@ -10,14 +10,14 @@ var connection = mysql.createPool({
   database: dbConfig.DB //  burgers_db
 });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 // Export connection for our ORM to use.
 module.exports = connection;
