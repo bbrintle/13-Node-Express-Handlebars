4f52096bb3c429d773056b59d460c67f52face13
bbrintle <bbrintle@gmail.com>
2020-11-05 14:26:26 -0800

    organizing and cleaning files

--- config/connection.js
+++ config/connection.js
@@ -3,21 +3,12 @@ var mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
 var connection = mysql.createPool({
-  host: dbConfig.HOST, //  localhost
+  host: dbConfig.HOST,
   port: 3306,
-  user: dbConfig.USER, //  root
-  password: dbConfig.PASSWORD, // <== Your Password Here!  password 
-  database: dbConfig.DB //  burgers_db
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB
 });
-
-// // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
 
 // Export connection for our ORM to use.
 module.exports = connection;
